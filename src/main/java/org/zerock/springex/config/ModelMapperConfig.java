3ed822fa80376465ac6816e5c31c70fd4e08b25b
sirorong <majestyj@naver.com>
2022-09-06 16:51:13 +0900

    20220906-1

---
+++ src/main/java/org/zerock/springex/config/ModelMapperConfig.java
@@ -0,0 +1,21 @@
+package org.zerock.springex.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
+    }
+}
---
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -0,0 +1,35 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.springex.dto.TodoDTO;
+
+@Controller
+@Log4j2
+@RequestMapping("/todo")
+public class TodoController {
+
+    @RequestMapping("/list")
+    public void list(){
+        log.info("todo list...................");
+    }
+
+    //@RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
+    public void registerGET(){
+        log.info("GET todo register...............");
+    }
+
+    @PostMapping("/register")
+    public void registerPOST(TodoDTO todoDTO ){
+        log.info("POST todo register......");
+        log.info(todoDTO);
+
+        log.info("POST todo register..............");
+
+    }
+}
